8b19fcbff35645780117f2bd3e53cf21c26c1ca4
wondering516 <1442265472@qq.com>
2020-03-14 00:42:22 +0800

    create IndexController

--- src/main/java/wang/lukas/springweb01/controller/IndexController.java
+++ src/main/java/wang/lukas/springweb01/controller/IndexController.java
@@ -1,21 +1,17 @@
 package wang.lukas.springweb01.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @Author: CoffeeChicken
- * @date: 2020/3/10 11:26
+ * @date: 2020/3/14 00:40
  */
 
 @Controller
-public class HelloController {
-
+public class IndexController {
     @GetMapping("/")
     public String index(){
-
         return "index";
     }
 }
